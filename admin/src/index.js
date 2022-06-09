9acd924f6895b99aae2cf5c5c4e3f88d6366041d
jaygandhi241 <62997537+jaygandhi241@users.noreply.github.com>
2022-06-09 19:43:03 +0530

    Update index.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { AuthContextProvider } from "./context/authContext/AuthContext";
-import { MovieContextProvider } from "./context/movieContext/MovieContext";
-import { ListContextProvider } from "./context/listContext/ListContext";
 
 ReactDOM.render(
   <React.StrictMode>
